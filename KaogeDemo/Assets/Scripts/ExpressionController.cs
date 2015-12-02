e344491d06505e38a4c3fb8d175030577da10501
Wu Yuntao <wyt.brandon@gmail.com>
2015-12-02 17:47:37 +0800

    Prototype of expression player

---
+++ KaogeDemo/Assets/Scripts/ExpressionController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpressionController : MonoBehaviour
+{
+    public Transform face;
+
+    public Transform leftEye;
+    public Transform leftEyebrow;
+    public Transform leftEyelid;
+    public Transform leftEyeball;
+
+    public Transform rightEye;
+    public Transform rightEyebrow;
+    public Transform rightEyelid;
+    public Transform rightEyeball;
+
+    public Transform lips;
+
+    public void Play(string expressionName)
+    {
+        Debug.LogFormat("Play expression '{0}'", expressionName);
+
+        switch (expressionName)
+        {
+            default:
+                foreach (var child in GetChildren())
+                {
+                    Play(child, expressionName);
+                }
+                break;
+        }
+    }
+
+    private void Play(Transform child, string expressionName)
+    {
+        if (child == null)
+            return;
+
+        var animator = child.GetComponent<Animator>();
+        if (animator == null)
+            return;
+
+        animator.Play(expressionName);
+    }
+
+    private IEnumerable<Transform> GetChildren()
+    {
+        yield return face;
+        yield return leftEye;
+        yield return rightEye;
+        yield return lips;
+    }
+}
---
+++ KaogeDemo/Assets/Scripts/UIEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UIEventHandler : MonoBehaviour
+{
+    public ExpressionController head;
+
+    public void OnClick(string expressionName)
+    {
+        head.Play(expressionName);
+    }
+}
